60a4be72fb60a64b2a15d78fc3d45d6cd56ea178
林新恩 <xlin9857@gmail.com>
2025-06-19 15:33:59 +0800

    update db.php

--- config/db.php
+++ config/db.php
@@ -1,12 +1,16 @@
 <?php
+header('Content-Type: application/json');
+
 $serverName = getenv('DB_HOST');
 $port = getenv('DB_PORT');
 $database = getenv('DB_NAME');
 $username = getenv('DB_USER');
 $password = getenv('DB_PASS');
 
+$dsn = "sqlsrv:Server=$server,$port;Database=$database;Encrypt=yes;TrustServerCertificate=yes"; // 加上 TrustServerCertificate=yes
+
 try {
-    $conn = new PDO("sqlsrv:Server=$serverName,$port;Database=$database", $username, $password, [
+    $conn = new PDO($dsn, $username, $password, [
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
     ]);
 } catch (PDOException $e) {
