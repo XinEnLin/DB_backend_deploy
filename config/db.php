e22b92784161b871b5a833c5ca8bc910b7f65f58
林新恩 <xlin9857@gmail.com>
2025-06-19 13:27:15 +0800

    update db.php

--- config/db.php
+++ config/db.php
@@ -10,7 +10,7 @@ $dbPass = getenv('DB_PASS');
 
 try {
     // 使用 PDO_SQLSRV 建立連線
-    $dsn = "sqlsrv:Server=$server,$port;Database=$dbName";
+    $dsn = "sqlsrv:Server=$server,$port;Database=$dbName;Encrypt=true;TrustServerCertificate=true";
     $options = [
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, // 啟用錯誤拋出
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
