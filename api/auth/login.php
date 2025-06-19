be635a9c94de99a5baef2a13ed86715d203e6e55
林新恩 <xlin9857@gmail.com>
2025-06-19 13:01:53 +0800

    部署

--- api/auth/login.php
+++ api/auth/login.php
@@ -1,5 +1,5 @@
 <?php
-header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Origin: https://db-finalproject.onrender.com");
 header("Access-Control-Allow-Credentials: true");
 header("Access-Control-Allow-Headers: Content-Type");
 header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
